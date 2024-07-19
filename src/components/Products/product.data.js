d832ad1619aa4f7c7322eaa19931fbc93a2065d9
Jeslin Marium Alex <jeslinmariumalex@gmail.com>
2024-07-19 12:30:10 +0530

    Uploading Files

---
+++ src/components/Products/product.data.js
@@ -0,0 +1,46 @@
+export const phonesData = [
+    {
+        id : 1,
+        model : "Realme 12 Pro+ 5G",
+        desc : "Realme 12 Pro+ 5G (Submarine Blue, 128 GB)",
+        price : 29999,
+        image : "realme",
+        qty : 1,
+    },
+
+    {
+        id : 2,
+        model : "POCO X6 5G",
+        desc : "POCO X6 5G (Snowstorm White, 256 GB)  (8 GB RAM)",
+        price : 19999,
+        image : "poco",
+        qty : 1,
+    },
+
+    {
+        id : 3,
+        model : "Vivo V29 5G",
+        desc : "Vivo V29 5G (Red, 128 GB)  (8 GB RAM)",
+        price : 32999,
+        image : "vivo",
+        qty : 1,
+    },
+
+    {
+        id : 4,
+        model : "Motorola g84 5G",
+        desc : "Motorola g84 5G (Viva Magenta, 256 GB)  (12 GB RAM)",
+        price : 17999,
+        image : "motorola",
+        qty : 1,
+    },
+
+    {
+        id : 5,
+        model : "Redmi Note 13 Pro 5G",
+        desc : "Redmi Note 13 Pro 5G (Coral Purple, 128 GB)  (8 GB RAM)",
+        price : 25999,
+        image : "redmi",
+        qty : 1,
+    },
+]
